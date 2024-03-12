770e1fac808d817b6a048d059274671871a2351b
logan <loganlenevez@gmail.com>
2024-03-12 10:58:36 +0100

    Refactor Win Condition for Morpion

--- 1-PuissanceQuatre/Morpion.cs
+++ 1-PuissanceQuatre/Morpion.cs
@@ -149,15 +149,40 @@ namespace MorpionApp
             Console.WriteLine($" {grille[2, 0]}  |  {grille[1, 1]}  |  {grille[0, 2]}");
         }
 
-        public bool verifVictoire(char c) =>
-             grille[0, 0] == c && grille[1, 0] == c && grille[2, 0] == c ||
-             grille[0, 1] == c && grille[1, 1] == c && grille[2, 1] == c ||
-             grille[0, 2] == c && grille[1, 2] == c && grille[2, 2] == c ||
-             grille[0, 0] == c && grille[1, 1] == c && grille[2, 2] == c ||
-             grille[1, 0] == c && grille[1, 1] == c && grille[1, 2] == c ||
-             grille[2, 0] == c && grille[2, 1] == c && grille[2, 2] == c ||
-             grille[0, 0] == c && grille[1, 1] == c && grille[2, 2] == c ||
-             grille[2, 0] == c && grille[1, 1] == c && grille[0, 2] == c;
+        private bool CheckRowWin(char playerSymbol)
+        {
+            for (int row = 0; row < 3; row++)
+            {
+                if (grille[row, 0] == playerSymbol && grille[row, 1] == playerSymbol && grille[row, 2] == playerSymbol)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private bool CheckColumnWin(char playerSymbol)
+        {
+            for (int col = 0; col < 3; col++)
+            {
+                if (grille[0, col] == playerSymbol && grille[1, col] == playerSymbol && grille[2, col] == playerSymbol)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private bool CheckDiagonalWin(char playerSymbol)
+        {
+            return (grille[0, 0] == playerSymbol && grille[1, 1] == playerSymbol && grille[2, 2] == playerSymbol) ||
+                (grille[0, 2] == playerSymbol && grille[1, 1] == playerSymbol && grille[2, 0] == playerSymbol);
+        }
+
+        public bool verifVictoire(char playerSymbol)
+        {
+            return CheckRowWin(playerSymbol) || CheckColumnWin(playerSymbol) || CheckDiagonalWin(playerSymbol);
+        }
 
         public bool verifEgalite() =>
             grille[0, 0] != ' ' && grille[1, 0] != ' ' && grille[2, 0] != ' ' &&
