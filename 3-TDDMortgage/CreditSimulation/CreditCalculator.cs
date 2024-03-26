95f44b32c24be647b04ae188839b52e43b52512c
logan <loganlenevez@gmail.com>
2024-03-26 20:37:34 +0100

    Third test passed

--- 3-TDDMortgage/CreditSimulation/CreditCalculator.cs
+++ 3-TDDMortgage/CreditSimulation/CreditCalculator.cs
@@ -1,6 +1,12 @@
 namespace CreditSimulation {
     public static class CreditCalculator {
+        private const decimal MinimumLoanAmount = 50000;
+        private const int MinimumDurationYears = 9;
+
         public static decimal CalculateMonthlyPayment(decimal loanAmount, int durationYears, decimal interestRate) {
+            if (loanAmount < MinimumLoanAmount) {
+                throw new ArgumentException($"Loan amount must be at least {MinimumLoanAmount}");
+            }
 
             int durationMonths = 12 * durationYears;
 
